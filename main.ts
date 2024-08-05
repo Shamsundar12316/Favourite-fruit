55d22cf65fe8e82ea3e33c7bbc193bc8e91faa7d
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 03:31:12 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,39 @@
+// part 24 // question 29// 
+// favourite fruit
+
+let favourite_fruit: string[] = ["mango","apple","grapes","banana","watermelon"]
+
+if(`${favourite_fruit[0]}`=== "mango"){
+    console.log(`I like mango`)
+}
+else{
+    console.log(`I dont like this fruit`)
+}
+if(`${favourite_fruit[1]}`=== "apple"){
+    console.log(`I like apple`)
+}
+else{
+    console.log(`I dont like this fruit`)
+}
+if(`${favourite_fruit[2]}`=== "grapes"){
+    console.log(`I like grapes`)
+}
+else{
+    console.log(`I dont like this fruit`)
+}
+
+
+
+if(`${favourite_fruit[3]}`=== "papaya"){
+    console.log(`I like papaya`)
+}
+else{
+    console.log(`I dont like this fruit`)
+}
+
+if(`${favourite_fruit[4]}`=== "peach"){
+    console.log(`I like peach`)
+}
+else{
+    console.log(`I dont like this fruit`)
+}
